fdb0c06601df364f0162a29f01ac0f508a954c2e
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-18 16:54:53 -0500

    01_ejr terminado

--- edd_shakmuria/01_ejr/src/main/java/company/App.java
+++ edd_shakmuria/01_ejr/src/main/java/company/App.java
@@ -13,19 +13,42 @@ import java.util.Scanner;
 public class App {
 
   // Scanner, MAX
-  static Scanner scanner =  new  Scanner(System.in);
+  static Scanner scanner = new Scanner(System.in);
   static final int MAX = 5;
-  
+
   public static void main(String[] args) {
-    int num;
     int arreglo[] = {1, 4, 45, 67, 43};
 
+    // Llamando funcion
+    buscar(arreglo);
   }
 
-  static int ingresarNumero(int arreglo[]) {
-    System.out.println("Buscar un numero\n");
-    System.out.println("Ingrese el numero a buscar: ");
+  static int ingresarNumero() {
+    System.out.println("Buscar un numero");
+    System.out.print("Ingrese numero a buscar o ingrese 0 para salir: ");
     int num1 = scanner.nextInt();
     return num1;
+  }
+
+  static void buscar(int arreglo[]) {
+    boolean encontrado = false;
+    int j=0, num1;
+    do {
+      num1 = ingresarNumero();
+      for (int i = 0; i < MAX; i++) {
+        j = i;
+        if (arreglo[i]==num1) {
+          encontrado = true;
+          break;
+        } else {
+          encontrado = false;
+        }
+      }
+      if (encontrado == true) {
+        System.out.println("\nNumero "+num1+" encontrado en la posicion " + (j+1)+"\n");
+      } else {
+        System.out.println("\nNumero no encontrado\n");
+      }
+    } while (num1 != 0);
   }
 }
