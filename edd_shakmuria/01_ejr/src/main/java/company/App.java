44d054f599f1f85617e91e9b3ef170ba9edad458
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-17 23:58:17 -0500

    Creando funcion. Ingresar numero

--- edd_shakmuria/01_ejr/src/main/java/company/App.java
+++ edd_shakmuria/01_ejr/src/main/java/company/App.java
@@ -4,20 +4,28 @@
  */
 package company;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jackh
  */
 public class App {
 
+  // Scanner, MAX
+  static Scanner scanner =  new  Scanner(System.in);
+  static final int MAX = 5;
+  
   public static void main(String[] args) {
+    int num;
     int arreglo[] = {1, 4, 45, 67, 43};
 
   }
 
-  static void ingresarNumero() {
+  static int ingresarNumero(int arreglo[]) {
     System.out.println("Buscar un numero\n");
     System.out.println("Ingrese el numero a buscar: ");
-    
+    int num1 = scanner.nextInt();
+    return num1;
   }
 }
