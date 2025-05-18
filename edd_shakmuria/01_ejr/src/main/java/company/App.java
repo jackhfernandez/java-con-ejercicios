e1ccb19205ab678de07d3d606c53bd24e00225be
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-17 23:47:53 -0500

    Curso estructura de datos

---
+++ edd_shakmuria/01_ejr/src/main/java/company/App.java
@@ -0,0 +1,19 @@
+/**
+ * Estructura de datos con shakmuria. 
+ * Programa: crear un arreglo con 5 elementos enteros y buscar uno de ellos
+ */
+package company;
+
+/**
+ *
+ * @author jackh
+ */
+public class App {
+
+    public static void main(String[] args) {
+      int arreglo[] = new int[5];
+        System.out.println("Buscar un numero en el arreglo");
+      for (int i = 0; i < 5; i++) {
+      }
+    }
+}
