77d8ecf2faac6f0233a2c5739bc81b40fcc621f3
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-05-17 23:54:39 -0500

    Creando 01_ejr

--- edd_shakmuria/01_ejr/src/main/java/company/App.java
+++ edd_shakmuria/01_ejr/src/main/java/company/App.java
@@ -1,5 +1,5 @@
 /**
- * Estructura de datos con shakmuria. 
+ * Estructura de datos con shakmuria.
  * Programa: crear un arreglo con 5 elementos enteros y buscar uno de ellos
  */
 package company;
@@ -10,10 +10,14 @@ package company;
  */
 public class App {
 
-    public static void main(String[] args) {
-      int arreglo[] = new int[5];
-        System.out.println("Buscar un numero en el arreglo");
-      for (int i = 0; i < 5; i++) {
-      }
-    }
+  public static void main(String[] args) {
+    int arreglo[] = {1, 4, 45, 67, 43};
+
+  }
+
+  static void ingresarNumero() {
+    System.out.println("Buscar un numero\n");
+    System.out.println("Ingrese el numero a buscar: ");
+    
+  }
 }
